f70d1c9eee11e38aa2d1274ec22d647c403a956b
Benjamin McGregor <bwtfm1@student.le.ac.uk>
2022-02-02 15:06:29 +0000

    ui configuration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import platform
 """
 This is the where the main loop occurs
 
@@ -14,10 +15,10 @@ class Launcher:
         self.master = master
         self.frame = tk.Frame(self.master)
 
-        self.start_button = tk.Button(self.frame, text="Start Game", command=self.start)
+        self.start_button = tk.Button(self.frame, text="Start Game", command=self.start, font=(gameFont, 50))
         self.start_button.pack()
 
-        self.options_button = tk.Button(self.frame, text="Options", command=self.options)
+        self.options_button = tk.Button(self.frame, text="Options", command=self.options, font=(gameFont, 50))
         self.options_button.pack()
 
         # These are just to avoid Pycharm warnings
@@ -51,21 +52,23 @@ class UngradingSimulator:
         self.master = master
         self.frame = tk.Frame(self.master)
 
-        self.testLabel = tk.Label(self.frame, text="Ungrading Simulator goes here.")
+        self.testLabel = tk.Label(self.frame, text="Ungrading Simulator goes here.", font=(gameFont, 20))
         self.testLabel.pack()
 
-        self.testEventButton = tk.Button(self.frame, text="Test Event", command=self.event)
+        self.testEventButton = tk.Button(self.frame, text="Test Event", command=self.event, font=(gameFont, 20))
         self.testEventButton.pack()
 
-        self.level_up_button=tk.Button(self.frame, text="Level Up test", command=self.levelUp)
+        self.level_up_button = tk.Button(self.frame, text="Level Up test", command=self.levelUp, font=(gameFont, 20))
         self.level_up_button.pack()
 
         # Ignore these two they just stop Pycharm from moaning
         self.eventScreen = None
         self.app = None
+        self.level_up_string = None
+        self.level_up_box = None
 
         self.frame.pack()
-        self.user_level=0 #user's level will be automatically 0, they've not learned anything
+        self.user_level = 0  # user's level will be automatically 0, they've not learned anything
 
     # Method to call and configure an event (will pick a random one later)
     def event(self):
@@ -91,7 +94,7 @@ class OptionsScreen:
         self.master = master
         self.frame = tk.Frame(self.master)
 
-        self.testLabel = tk.Label(self.frame, text="Options go here.")
+        self.testLabel = tk.Label(self.frame, text="Options go here.", font=(gameFont, 20))
         self.testLabel.pack()
 
         self.frame.pack()
@@ -103,10 +106,10 @@ class Event:
         self.master = master
         self.frame = tk.Frame(self.master)
 
-        self.eventTitleLabel = tk.Label(self.frame, text=event_title)
+        self.eventTitleLabel = tk.Label(self.frame, text=event_title, font=(gameFont, 15))
         self.eventTitleLabel.pack()
 
-        self.eventTextLabel = tk.Label(self.frame, text=event_text)
+        self.eventTextLabel = tk.Label(self.frame, text=event_text, font=(gameFont, 15))
         self.eventTextLabel.pack()
 
         self.frame.pack()
@@ -118,9 +121,25 @@ def main():
     app = Launcher(root)
     root.geometry("1280x720")
     root.title("Splash Screen")
+    root.option_add('*Dialog.msg.font', 'Helvetica 15')  # Sets dialogue message font
     root.mainloop()
+
+
+# Method to set the games font based on the OS
+def set_game_font():
+    if platform.system() == "Linux":  # Linux builds
+        return "Helvetica"
+    elif platform.system() == "Darwin":  # MacOS
+        return "Gill Sans"
+    elif platform.system() == "Windows":  # Microsoft Windows
+        return "Segoe UI"
+    else:
+        return "Arial"  # In case the system cannot be identified
 
 
 # Runs main method
 if __name__ == "__main__":
+    global gameFont
+    gameFont = set_game_font()
+
     main()
