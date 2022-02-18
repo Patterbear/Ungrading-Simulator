8d47ea1de9e8f8db92877134e1c1d8d59522ac13
A P Singh <aps32@student.le.ac.uk>
2022-02-18 11:11:14 +0000

    Update Submission_File.py

--- Submission_File.py
+++ Submission_File.py
@@ -1,12 +1,15 @@
 from cgitb import text
+from distutils import command
 from tkinter import *
 import tkinter as tk
 import tkinter.font as font
+from tkinter import messagebox
+from turtle import width
 
 # creating a window
 main = Tk()
 main.title("Ungrading Simulator")
-main.iconbitmap("Images/app_icon.png")
+#main.iconbitmap("Images/app_icon.png")
 main.geometry('800x800')
 
 font1 = font.Font(family='Calibri Bold', size=25)
@@ -25,6 +28,11 @@ bframe.pack(side=BOTTOM)
 
 # Commands
 # Print
+def sub_message():
+    return messagebox.askokcancel("Thanks for submitting Activity 1", "You have shown good understanding in Activity 1. Therefore, you are awarded 73% in Activity 1")
+
+
+
 # def take_input1():
 #     inputValue = name_input.get("1.0",END)
 #     print(inputValue)
@@ -32,11 +40,12 @@ bframe.pack(side=BOTTOM)
 
 # CREATING WIDGETS/BUTTONS
 submit_btn = Button(bframe, text='Submit', fg='Black', width=20, height=1, font=font2, borderwidth=2,
-                    command=main.quit)
+                    command=sub_message)
 submit_btn.pack(side=RIGHT)
-cancel_btn = Button(bframe, text='Cancel', fg='Black', width=20, height=1, font=font2, borderwidth=2, command=main.quit)
+cancel_btn = Button(bframe, text='Close', fg='Black', width=20, height=1, font=font2, borderwidth=2, command=main.quit)
 cancel_btn.pack(side=RIGHT)
-
+main_btn= Button (bframe, text= "Main Menu", fg="Black", width=20, height=1, font= font2, borderwidth=2, command= main.quit)
+main_btn.pack(side=RIGHT)
 # Name Labels
 name_label = Label(main, text="Full Name", font=font2)
 name_label.place(x=40, y=60)
@@ -88,5 +97,7 @@ comment_box.place(x=40, y=340)
 click_btn = Button(main, height=1, width=10, text="Click", command=lambda: take_input())
 click_btn.pack()
 click_btn.place(x=40, y=520)
+
+
 
 mainloop()
