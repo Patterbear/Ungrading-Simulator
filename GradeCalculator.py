23ce6ad0e99e5f52980ced9dcc197d452f1e7642
Benjamin McGregor <bwtfm1@student.le.ac.uk>
2022-03-25 19:12:56 +0000

    amended grade calculator to adhere to client specification

--- GradeCalculator.py
+++ GradeCalculator.py
@@ -3,58 +3,68 @@ from cgitb import text
 from logging import root
 from textwrap import fill
 from tkinter import *
+from tkinter import ttk
 from turtle import heading
 from main import set_game_font
 import random
+from PIL import Image, ImageTk
 
 g_skill_level = 0
 g_total_activities = 0
 
 
-def run(skill_level, total_activities):
+def run(avatar, skill_level, total_activities, awareness):
 
     global g_skill_level
     global g_total_activities
+    global g_avatar
+    global g_awareness
 
     g_skill_level = skill_level
     g_total_activities = total_activities
+    g_awareness = awareness
+
+    if avatar is None:
+        g_avatar = "assets/default_character.gif"
+    else:
+        g_avatar = avatar
 
     gameFont = set_game_font()
 
     root = tkinter.Toplevel()
     root.title('Grade Calculator')
-    root.geometry('400x400')
-    root.configure(background='light grey')
+    root.geometry('500x600')
 
+    # Function to calculate final score and place it on the screen
     def calculate():
-        #c_level= int(label1_entry.get())
         skill = g_skill_level
         a_completed = g_total_activities
-        s_grade = int(label4_entry.get())
-        g_achieved= str((g_skill_level*10) + random.randint(0, 6)) + "%"
-        Label(root, text=g_achieved, font='Gamefont 18 bold').place(x=190,y=275)
-        Button(root, text='End Game', font=(gameFont, 10), bg='white', fg='black', command=quit).place(x=50, y=320)
+        g_achieved= (g_skill_level*10) + random.randint(0, 6)
 
-    heading_label = Label(root, text='Self Assessment ', font='Gamefont 18 bold',bg='grey',fg='black').pack()
-    #label1 = Label(root, text='Confidence Level ',font='Gamefont 8 bold').place(x=50,y=70)
-    label2 = Label(root, text='Skill Level ',font='Gamefont 8 bold').place(x=70,y=105)
-    #label3 = Label(root, text='Completed Activities ',font='Gamefont 8 bold').place(x=40,y=140)
-    label4 = Label(root, text='Suggested Grade  ',font='Gamefont 8 bold').place(x=50,y=195)
-    label5 = Label(root, text='Total',font='Gamefont 8 bold').place(x=85,y=240)
-    label6 = Label(root, text='Grade Achieved',font='Gamefont 8 bold',).place(x=55,y=280)
+        # Determines colour of grade label based on grade (green for good, yellow for ok and red for bad)
+        if g_achieved >= 70:
+            score_colour = 'green'
+        elif g_achieved <= 69 and g_achieved > 50:
+            score_colour = 'yellow'
+        else:
+            score_colour = 'red'
 
-    #label1_entry = Entry(root,font="8",width='15',bd='2')
-    #label1_entry.place(x=190,y=70)
+        Label(root, font=(gameFont, 18)).pack()
+        Label(root, text="You scored: " + str(g_achieved) + "%", font=(gameFont, 40), bg=score_colour).pack()#place(x=190,y=275)
+        Label(root, font=(gameFont, 18)).pack()
+        Button(root, text='End Game', font=(gameFont, 15), command=quit).pack()
+        Button(root, text='Get my grade', font=(gameFont, 20), state="disabled").place(x=152, y=330)
 
-    label2_entry = Label(root, font="10", width='15', text=str(skill_level))
-    label2_entry.place(x=190,y=105)
 
-    #label3_entry = Label(root, font="10", width='15', text=str(total_activities))
-    #label3_entry.place(x=190,y=140)
+    heading_label = Label(root, text='Self Assessment', font=(gameFont, 18)).pack()
 
-    label4_entry = Entry(root,font="10",width='15',bd='2')
-    label4_entry.place(x=190,y=195)
+    img = ImageTk.PhotoImage(Image.open(g_avatar))
+    profile_image = Label(root, image=img)
+    profile_image.pack()
 
-    calculate_button = Button(root,text='Calculate ',font=(gameFont, 10),bg='white',fg='black',command=calculate).place(x=50,y=320)
-    exit_button = Button(root,text='Exit ',font='Gamefont 10 bold',bg='white',fg='black', width= '15',command=exit).place(x=220,y=320)
+    character_dialogue = Label(root, bg='white', font=(gameFont, 18), text='"I reckon I achieved a grade of of ' + str(int(g_awareness*((g_skill_level*10) + random.randint(0, 6)))) + '%"').pack()
+    Label(root, font=(gameFont, 18)).pack()
+
+    calculate_button = Button(root, text='Get my grade', font=(gameFont, 20), command=calculate).pack()
+
     root.mainloop()
